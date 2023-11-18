f734d241ac8fb41566aab296865ebdc46af60933
Hyeonjeong Lee <hlee220@myseneca.ca>
2023-11-18 15:42:52 +0900

    모바일 gnb 작업 완료

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -4,49 +4,15 @@
 //resize: 브라우저 창 너비의 변경된 값을 width 변수에 저장
 $(window).resize(function () {
     var width = $(window).width();
-    if (width <= 1024) {
-        $(".sitemap-button").click(function () {
-            $(this).toggleClass("is-active");
-            $("header").toggleClass("is-open");
-            $('body, html').toggleClass("is-hidden");
+    if (width <= 720) {
+        $(".gnb-button").click(function () {
+            $(this).closest(".gnb").addClass("is-active");
         });
-        // $(".sitemap-lists__title").on('click', function(){
-        //     //$(this).parents(".gnb-menu__list").toggleClass("is-open");
-        //     $(this).parents('.sitemap-list').toggleClass("is-open");
-        //     $(this).next().slideToggle();
-        //     $(this).parents().siblings().children(".sitemap-list__item").slideUp();
-        //     //$(this).parents().siblings().removeClass("is-open");
-        // });
-        $(".sitemap-close__button").click(function () {
-            $('.sitemap-button').removeClass("is-active");
-            $("header").removeClass("is-open");
-            $('body, html').removeClass("is-hidden");
+        $(".gnb-menu .gnb-button").click(function () {
+            $(this).closest(".gnb").removeClass("is-active");
         });
-
     } else {
-        //header open
-       
-
-        $(".sitemap-button").click(function () {
-            $('.sitemap-content').addClass('is-open');
-            $('body').addClass('is-hidden');
-        });
-        $(".sitemap-close__button").click(function () {
-            $('.sitemap-content').removeClass('is-open');
-            $('body').removeClass('is-hidden');
-        });
-
-        //header close
-        $(".gnb-menu").mouseleave(function () {
-            $(this).removeClass("is-open");
-            $("header").removeClass("is-open");
-
-            if (($("header").hasClass("is-open") === true)) {
-
-            } else {
-                $(".gnb-menu__list").removeClass("is-active");
-            }
-        });
+        
     }
 });
 
@@ -55,41 +21,16 @@ $(window).trigger("resize"); //강제로 호출하는 함수
 
 $(function () {
     var width = $(window).width();
-    if (width <= 1024) {
-        $(".sitemap-button").click(function () {
-            $(this).toggleClass("is-active");
-            $("header").toggleClass("is-open");
-            $('body, html').toggleClass("is-hidden");
+    if (width <= 720) {
+        $(".gnb-button").click(function () {
+            $(this).closest(".gnb").addClass("is-active");
         });
-        
-        $(".sitemap-close__button").click(function () {
-            $('.sitemap-button').removeClass("is-active");
-            $("header").removeClass("is-open");
-            $('body, html').removeClass("is-hidden");
+        $(".gnb-menu .gnb-button").click(function () {
+            $(this).closest(".gnb").removeClass("is-active");
         });
-
+     
     } else {
       
-        $(".sitemap-button").click(function () {
-            $('.sitemap-content').addClass('is-open');
-            $('body').addClass('is-hidden');
-        });
-        $(".sitemap-close__button").click(function () {
-            $('.sitemap-content').removeClass('is-open');
-            $('body').removeClass('is-hidden');
-        });
-
-        //header close
-        $(".gnb-menu").mouseleave(function () {
-            $(this).removeClass("is-open");
-            $("header").removeClass("is-open");
-
-            if (($("header").hasClass("is-open") === true)) {
-
-            } else {
-                $(".gnb-menu__list").removeClass("is-active");
-            }
-        });
     }
 
     
